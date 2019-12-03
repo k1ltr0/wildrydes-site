6fde5775d612a27120539fb54059d7bb9a06f5b0
Ubuntu <ubuntu@ip-172-31-24-51.us-west-2.compute.internal>
2019-12-03 14:46:09 +0000

    added credentials

--- js/config.js
+++ js/config.js
@@ -1,8 +1,8 @@
 window._config = {
     cognito: {
-        userPoolId: '', // e.g. us-east-2_uXboG5pAb
-        userPoolClientId: '', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
-        region: '' // e.g. us-east-2
+        userPoolId: 'us-west-2_PKEEQwHZV', // e.g. us-east-2_uXboG5pAb
+        userPoolClientId: '7jabgg7vlr8pumtd09pbatahof', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
+        region: 'us-west-2' // e.g. us-east-2
     },
     api: {
         invokeUrl: '' // e.g. https://rc7nyt4tql.execute-api.us-west-2.amazonaws.com/prod',
